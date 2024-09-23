54d28352c9293e5df70aa16abb407d80d0797e10
Ponnarasu <ponnarasua22it@srishakthi.ac.in>
2024-09-23 11:48:04 +0530

    profile change in sample-2

--- sample-2/src/components/Navbar.jsx
+++ sample-2/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Cross, User2, User2Icon, CircleX } from 'lucide-react'
-import React, {useRef, useState} from 'react'
+import React, { useRef, useState } from 'react'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
@@ -58,7 +58,7 @@ const Navbar = () => {
                                     <div className="w-1/2 flex justify-end">
                                         <label className="relative inline-flex items-center cursor-pointer" onClick={() => setvisible(!visible)} >
                                             <input type="checkbox" value="" className="sr-only peer" />
-                                            <CircleX className="peer ring-0 bg-slate-500  rounded-full outline-none duration-300 after:duration-500 w-8 h-8  shadow-md peer-checked:bg-emerald-500  peer-focus:outline-none  after:rounded-full after:absolute after:outline-none after:h-6 after:w-6 after:bg-white after:top-1 after:left-1 after:flex after:justify-center after:items-center  peer-hover:after:scale-75 peer-checked:after:content-['✔️'] after:-rotate-180 peer-checked:after:rotate-0"/>                                        
+                                            <CircleX className="peer ring-0 bg-slate-500  rounded-full outline-none duration-300 after:duration-500 w-8 h-8  shadow-md peer-checked:bg-emerald-500  peer-focus:outline-none  after:rounded-full after:absolute after:outline-none after:h-6 after:w-6 after:bg-white after:top-1 after:left-1 after:flex after:justify-center after:items-center  peer-hover:after:scale-75 peer-checked:after:content-['✔️'] after:-rotate-180 peer-checked:after:rotate-0" />
                                         </label>
                                     </div>
                                 </div>
--- sample-2/src/components/footer.jsx
+++ sample-2/src/components/footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="fixed bottom-0 w-full bg-slate-400 text-center py-2">
+        <footer className=" flex justify-center items-center fixed bottom-0 w-full bg-slate-400 text-center py-2">
             <p>&copy; 2023 Your Company. All rights reserved.</p>
         </footer>
     )
